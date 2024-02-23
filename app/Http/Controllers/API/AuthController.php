de75282dad95aef46830f9363a38cbcfb5beae44
Vl <vlehayrapetyan77@gmail.com>
2024-02-23 16:38:14 +0400

    fix userResource

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\AuthUserResource;
-use App\Http\Resources\UserResource;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
