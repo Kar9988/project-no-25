86f81441128de9e8a7e1dd986c53c5aa13dd4e71
Kar <karmn9988@gmail.com>
2024-02-26 15:00:17 +0400

    Fixed config path

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
         vue(),
 
         laravel({
-            buildDirectory: 'public/build',
+            buildDirectory: 'build',
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
