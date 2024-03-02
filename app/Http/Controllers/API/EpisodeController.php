355048de52ceb33a904311383c153683987b9e4a
Kar <karmn9988@gmail.com>
2024-03-02 21:36:51 +0400

    Fix

--- app/Http/Controllers/API/EpisodeController.php
+++ app/Http/Controllers/API/EpisodeController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Services\EpisodeService;
+use App\Services\VideoStream;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Response;
 
 class EpisodeController extends Controller
 {
@@ -21,9 +24,25 @@ class EpisodeController extends Controller
 
     public function getVideoStream($episodeId)
     {
+
         $episode = $this->episodeService->getById($episodeId);
 
-        \Iman\Streamer\VideoStreamer::streamFile(public_path("storage/$episode->video_path"));
+        $videoPath = public_path("storage/$episode->source");
+
+        $stream = new VideoStream($videoPath);
+        $response = Response::stream(function () use ($stream) {
+            $stream->start();
+        }, 200, [
+            'Content-Type' => 'video/mp4',
+            'Cache-Control' => 'no-cache, no-store, must-revalidate',
+            'Pragma' => 'no-cache',
+            'Expires' => '0',
+            'Content-Range' => 1000,
+            'Content-Disposition' => 'inline',
+            'X-Content-Type-Options' => 'nosniff',
+        ]);
+
+        $response->send();
     }
 
 }
