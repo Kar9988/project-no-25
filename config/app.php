cee978d4057d61f8c4368d26c95a8c0d8dc0bc31
Kar <karmn9988@gmail.com>
2024-03-09 20:44:13 +0400

    Fix

--- config/app.php
+++ config/app.php
@@ -183,7 +183,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
-        'Image' => Intervention\Image\Image::class,
+        'Image' => Intervention\Image\ImageManager::class,
     ])->toArray(),
 
 ];
