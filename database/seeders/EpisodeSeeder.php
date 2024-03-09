3e152f442f215904a5803baa5aef71998cc727f4
Kar <karmn9988@gmail.com>
2024-03-09 20:01:42 +0400

    Image compress

--- database/seeders/EpisodeSeeder.php
+++ database/seeders/EpisodeSeeder.php
@@ -2,10 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use App\Services\VideoService;
 use Illuminate\Database\Seeder;
 use Faker\Factory as Faker;
 use Illuminate\Http\File;
+use Illuminate\Support\Facades\DB;
 
 class EpisodeSeeder extends Seeder
 {
@@ -26,20 +28,39 @@ class EpisodeSeeder extends Seeder
      */
     public function run(): void
     {
+        $categoryData = [
+            [
+                'name'   => 'New Arrival',
+                'active' => true
+            ],
+            [
+                'name'   => 'Top Rated',
+                'active' => true
+            ],
+            [
+                'name'   => 'Trading This week',
+                'active' => true
+            ],
+            [
+                'name'   => 'Test',
+                'active' => true
+            ]
+        ];
+        DB::table('categories')->insert($categoryData);
         $faker = Faker::create();
         for ($i = 0; $i < 20; $i++) {
             $videoData = [
                 'title'       => $faker->name,
                 'description' => $faker->text,
                 'category_id' => rand(1, 4),
-                'cover_img' => glob(base_path('test_thumbs/*'))[rand(0, 2)],
-                'episodes'  => []
+                'cover_img'   => glob(base_path('test_thumbs/*'))[rand(0, 2)],
+                'episodes'    => []
             ];
             for ($x = 0; $x < 20; $x++) {
                 $videoData['episodes'][] = [
                     'title'    => $faker->name,
                     'duration' => null,
-                    'position' => $x+1,
+                    'position' => $x + 1,
                     'price'    => 120,
                     'thumb'    => glob(base_path('test_thumbs/*'))[rand(0, 2)],
                     'source'   => new File(glob(base_path('test_videos/*'))[rand(0, 4)]),
