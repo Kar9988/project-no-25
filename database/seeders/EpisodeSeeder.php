5e5d2faf3839f9acc2c0a371ce35861f2114f646
Kar <karmn9988@gmail.com>
2024-03-11 23:08:11 +0400

    Modified

--- database/seeders/EpisodeSeeder.php
+++ database/seeders/EpisodeSeeder.php
@@ -28,6 +28,27 @@ class EpisodeSeeder extends Seeder
      */
     public function run(): void
     {
-
+        $faker = Faker::create();
+        for ($i = 0; $i < 5; $i++) {
+            $videoData = [
+                'title'       => $faker->name,
+                'description' => $faker->text,
+                'category_id' => rand(1, 4),
+                'cover_img'   => glob(base_path('test_thumbs/*'))[rand(0, 2)],
+                'episodes'    => []
+            ];
+            for ($x = 0; $x < 3; $x++) {
+                $videoData['episodes'][] = [
+                    'title'    => $faker->name,
+                    'duration' => null,
+                    'position' => $x + 1,
+                    'price'    => 120,
+                    'thumb'    => glob(base_path('test_thumbs/*'))[rand(0, 2)],
+                    'source'   => new File(glob(base_path('test_videos/*'))[rand(0, 4)]),
+                ];
+            }
+            $result = $this->videoService->createVideo($videoData);
+            dump($result->id);
+        }
     }
 }
