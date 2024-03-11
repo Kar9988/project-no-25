e76e4ffe13eb45dce73cca3bb447d543beebd90d
Vl <vlehayrapetyan77@gmail.com>
2024-03-11 21:20:55 +0400

    add bonus column to user balances table

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -19,6 +19,7 @@ class UserResource extends JsonResource
             'name'       => $this->name,
             'email'      => $this->email,
             'balance'    => $this->userBalance->amount ?? 0,
+            'bonus'      => $this->userBalance->bonus,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
         ];
