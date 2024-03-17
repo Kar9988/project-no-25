8898f92fe0f9f8b07a2e99a877e8a36fabaed9ea
Har <halaverdyan1995@gmail.com>
2024-03-17 20:45:23 +0400

    Fix

--- resources/views/email/email.blade.php
+++ resources/views/email/email.blade.php
@@ -4,8 +4,6 @@
     <title>Example Email</title>
 </head>
 <body>
-<h2>Hello, {{ $user['first_name'] }}</h2>
-<h2>{{ $user['last_name'] }}</h2>
 <h2>{{ $user['email'] }}</h2>
 <h2>{{ $user['message'] }}</h2>
 
