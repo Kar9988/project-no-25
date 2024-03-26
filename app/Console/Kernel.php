b428076f84e7020a539ad63d3449ca51a09ae4de
Vl <vlehayrapetyan77@gmail.com>
2024-03-26 15:28:33 +0400

    fix time

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,7 +14,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-         $schedule->command('pay:invoice')->dailyAt('12:59');
+         $schedule->command('pay:invoice')->dailyAt('00:00');
     }
 
     /**
