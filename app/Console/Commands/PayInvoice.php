3816e933ff35396b1db7c03fdc77021b322d6561
Vl <vlehayrapetyan77@gmail.com>
2024-03-28 00:17:23 +0400

    fix command invoice

--- app/Console/Commands/PayInvoice.php
+++ app/Console/Commands/PayInvoice.php
@@ -4,12 +4,14 @@ namespace App\Console\Commands;
 
 
 use App\Models\Invoice;
+use App\Models\Plan;
 use App\Services\InvoiceService;
 use App\Services\PaymentService;
 use App\Services\SubscriptionService;
 use App\Services\UserBalanceService;
 use Illuminate\Console\Command;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class PayInvoice extends Command
 {
@@ -41,52 +43,61 @@ class PayInvoice extends Command
      */
     public function handle()
     {
-        $invoices = $this->invoiceService->getByDate(Carbon::today()->toDateString());
-        $data = [];
-        foreach ($invoices as $invoice) {
-            $data = [
-                'amount' => $invoice->subscription->plan->price,
-            ];
-            $paymentable = [
-                'from_id' => 0,
-                'user_id' => $invoice->subscription->user->id,
-                'amount' => $invoice->subscription->plan->price,
-                'paymentable_type' => Invoice::class,
-                'Paymentable_id' => $invoice->id
-            ];
-            $this->paymentService->create($paymentable);
-            $updateBalance = $this->userBalanceService->addBalance($data, $invoice->subscription->user_id, false);
-            if ($updateBalance['success']) {
-                $type = $invoice->subscription->plan['type'];
-                $updateSub = [
-                    'start_date' => Carbon::today()->toDateString(),
-                    'end_date' => Carbon::today()->toDateString(),
+        try {
+            DB::beginTransaction();
+            $invoices = $this->invoiceService->getByDate(Carbon::today()->toDateString());
+            $data = [];
+            foreach ($invoices as $invoice) {
+                $data = [
+                    'amount' => $invoice->subscription->plan->price,
                 ];
-
-                switch ($type) {
-                    case 'Weekly':
-                        $updateSub['end_date'] = Carbon::today()->addWeeks()->toDateString();
-                        break;
-                    case 'Yearly':
-                        $updateSub['end_date'] = Carbon::today()->addYear()->toDateString();
-                        break;
-                    case 'One Time':
-                        break;
-                }
-                $updateSubscription = $this->subService->update($invoice->subscription['id'], $updateSub);
-                if ($updateSubscription) {
-                    $invoiceData = [
-                        'user_id' => $invoice->subscription->user['id'],
-                        'subscription_id' => $invoice->subscription['user_id'],
-                        'next_attempt' => $invoice->subscription['end_date'],
-                        'amount' => $invoice->subscription->plan['price'],
+                $paymentable = [
+                    'from_id' => 0,
+                    'user_id' => $invoice->subscription->user->id,
+                    'amount' => $invoice->subscription->plan->price,
+                    'paymentable_type' => Plan::class,
+                    'Paymentable_id' => $invoice->subscription->plan->id
+                ];
+                $this->paymentService->create($paymentable);
+                $updateBalance = $this->userBalanceService->addBalance($data, $invoice->subscription->user_id, false);
+                if ($updateBalance['success']) {
+                    $type = $invoice->subscription->plan['type'];
+                    $updateSub = [
+                        'start_date' => Carbon::today()->toDateString(),
+                        'end_date' => Carbon::today()->toDateString(),
                     ];
-                    $this->invoiceService->addInvoice($invoiceData);
-                    $this->invoiceService->delete($invoice->id);
+
+                    switch ($type) {
+                        case 'Weekly':
+                            $updateSub['end_date'] = Carbon::today()->addWeeks()->toDateString();
+                            break;
+                        case 'Yearly':
+                            $updateSub['end_date'] = Carbon::today()->addYear()->toDateString();
+                            break;
+                        case 'One Time':
+                            break;
+                    }
+                    $updateSubscription = $this->subService->update($invoice->subscription['id'], $updateSub);
+                    if ($updateSubscription) {
+                        $invoiceData = [
+                            'user_id' => $invoice->subscription->user['id'],
+                            'subscription_id' => $invoice->subscription['user_id'],
+                            'next_attempt' => $invoice->subscription['end_date'],
+                            'amount' => $invoice->subscription->plan['price'],
+                        ];
+                        $this->invoiceService->addInvoice($invoiceData);
+                        $this->invoiceService->delete($invoice->id);
+                    }
                 }
             }
-
+            DB::commit();
+        }catch (\Exception $exception){
+            DB::rollBack();
+            return [
+                'success' => false,
+                'type'    => 'error',
+                'message' => $exception->getMessage()
+            ];
         }
-
     }
 }
