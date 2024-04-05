0ac84a5d544e6b9ed68216538ccfd2d1c006bb7f
Vl <vlehayrapetyan77@gmail.com>
2024-04-05 15:20:11 +0400

    delete SubscriptionPolicy

--- app/Http/Resources/AuthUserResource.php
+++ app/Http/Resources/AuthUserResource.php
@@ -1,8 +1,6 @@
 <?php
 
 namespace App\Http\Resources;
-
-use App\Policies\SubscriptionPolicy;
 use App\Policies\UserPolicy;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
